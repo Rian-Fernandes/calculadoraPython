d6df4f4be4131753b97f392a59fb46e138296a72
Rian-Fernandes <rian10fernandes@hotmail.com>
2023-11-09 20:19:36 -0300

    Declarando lógica básica

--- calculadora.py
+++ calculadora.py
@@ -1,4 +1,23 @@
 print("Digite um numero")
 numero1 = int(input())
 
-print
+print("Digite um operador")
+operador = input()
+
+print("Digite um segundo número")
+numero2 = int(input())
+
+if operador == '+':
+    resultado = numero1 + numero2
+    print(resultado)
+elif operador == '-':
+    resultado = numero1 - numero2
+    print(resultado)
+elif operador == '*':
+    resultado = numero1 * numero2
+    print(resultado)
+elif operador == '/':
+    resultado = numero1 / numero2
+    print(resultado)
+else:
+    print("Operador incorreto")
